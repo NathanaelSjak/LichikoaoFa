d595aa9227f414b36fdf0ac837337dda377d9b97
NathanaelSjak <154752189+NathanaelSjak@users.noreply.github.com>
2024-11-12 13:15:50 +0700

    ddd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gallery = [
     {
-        image: "/mnt/data/amiko.jpg",
+        image: "./mnt/data/amiko.jpg",
         title: "Amiko",
         description:
             "Ichiko’s score to the award winning film Amiko, featuring the theme song ‘Hello’ . Amiko is a movie  based on the debut novel written by Akutagawa Prize winning author Natsuko Imamura. Ichiko’s score won the 2022 Mainichi Film Award for Best Music.",
@@ -8,7 +8,7 @@ const gallery = [
         navColor: "#5A4634",
     },
     {
-        image: "/mnt/data/amuletum.jpg",
+        image: "./mnt/data/amuletum.jpg",
         title: "Amuletum Bouquet",
         description:
             "Ethereal and soothing: Ichiko Aoba brings a wave of calm with instruments that compliment the vocals, forming graceful melodies flowing from within both tracks.",
@@ -16,7 +16,7 @@ const gallery = [
         navColor: "#435961",
     },
     {
-        image: "/mnt/data/luminescent.jpg",
+        image: "./mnt/data/luminescent.jpg",
         title: "Luminescent Creature",
         description:
             "The world premiere of “Luminescent Creatures” will be held in Tokyo and Osaka later this year. A yet unannounced work, an ensemble of ten musicians will come together to perform this new project. Ichiko’s singles from 2020 onward will also be performed together for the first time ever!",
@@ -24,7 +24,7 @@ const gallery = [
         navColor: "#5A769E",
     },
     {
-        image: "/mnt/data/meringue.jpg",
+        image: "./mnt/data/meringue.jpg",
         title: "Meringue Doll",
         description:
             "In the depths of my memory There is a blossoming airship As the seasons sprout I travel, swaying in the wind Surely one day I'll find my way home So, just wait for me For your sadness, a song I savor the taste of you by my side When I hold you close Dappled sunlight dances, laughing",
@@ -32,7 +32,7 @@ const gallery = [
         navColor: "#598C5A",
     },
     {
-        image: "/mnt/data/windswept.jpg",
+        image: "./mnt/data/windswept.jpg",
         title: "Windswept Adan",
         description:
             "“Windswept Adan”,  Ichiko Aoba’s  seventh studio album,  is a singer-songwriter album, a concept album, a piece of chamber music, and a contemporary orchestral work. It is heavily influenced by jazz, by folk and by impressionistic classical music. It is a soundtrack to an imaginary film; it is an album that tells a story; it is a piece of fantasy science fiction set to music; it is a sonic voyage through the East China Sea.",
